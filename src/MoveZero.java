d003c00a4f92d043e5b45c1ae01d97abd645b1f8
sumitdvlp <skuma115@asu.edu>
2018-09-05 19:12:05 -0700

    Septe 5

--- src/MoveZero.java
+++ src/MoveZero.java
@@ -3,32 +3,30 @@
  */
 class PushZero
 {
-    static void moveZeros(int arr[])
+    static void moveZeros2(int arr[])
     {   int n = arr.length;
         int count = 0;
 
-        for (int i = 0; i < n; i++)
-            if (arr[i] != 0)
+        for (int i = 0; i < n; i++) // if the value in array is not 0 the we move it forward step
+            if (arr[i] != 0) //
                 arr[count++] = arr[i];
 
-        while (count < n)
+        while (count < n) // for rest of the digit put zero
             arr[count++] = 0;
     }
 
-    public static void moveZeros2(int[] nums) {
+    public static void moveZeros1(int[] nums) {
         int cntr=0,strt=0;
         for(int j=0,i=0;j<nums.length;j++){
-            if(nums[j]==0&&cntr==0){
+            if(nums[j]==0&&cntr==0){ // if nums index is zero store it in start
                 i=j;
                 strt=i;
                 cntr++;
-            }else if(nums[j]!=0){
+            }else if(nums[j]!=0){ // the nums index is not zero swap it with next number with start of zero
                 swap(nums,strt,j);
                 strt=strt+1;
-                i=j;
             }else if(nums[j]==0&&cntr!=0){
                 cntr++;
-                i=j;
             }
         }
     }
@@ -43,7 +41,7 @@ class PushZero
     {
         int arr[] = {0,1,0,3,12};
         int n = arr.length;
-        moveZeros2(arr);
+        moveZeros1(arr);
         for(int i:arr)
             System.out.print(i+", ");
     }
